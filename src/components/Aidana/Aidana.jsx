4c87461b51459e7c7361c5dec04208f63a08e60a
Ruslan <stamkulovruslan72@gmail.com>
2024-02-02 19:43:44 +0600

    changed Aidana

--- src/components/Aidana/Aidana.jsx
+++ src/components/Aidana/Aidana.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Aidana = () => {
-  return <div>Aidana</div>;
+  return (
+    <div>
+      <h2>Hello Aidana</h2>
+    </div>
+  );
 };
 
 export default Aidana;
